55899f7bf37286eac40ad14e8ee4128aff671d1b
unknown <angga.dharma60@gmail.com>
2022-12-19 17:27:52 +0700

    update

---
+++ stats.py
@@ -0,0 +1,90 @@
+# Created by: Michael Klements
+# For Raspberry Pi Desktop Case with OLED Stats Display
+# Base on Adafruit CircuitPython & SSD1306 Libraries
+# Installation & Setup Instructions - https://www.the-diy-life.com/add-an-oled-stats-display-to-raspberry-pi-os-bullseye/
+import time
+import board
+import busio
+import digitalio
+import psutil
+import os
+import sys
+import signal
+
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+import subprocess
+
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# Display Parameters
+WIDTH = 128
+HEIGHT = 64
+BORDER = 5
+
+# Use for I2C.
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+# Clear display.
+oled.fill(0)
+oled.show()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white background
+draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+font = ImageFont.truetype('PixelOperatorSC-Bold.ttf', 11)
+#font = ImageFont.load_default()
+padding = -2
+top = padding
+bottom = oled.height-padding
+x = 0
+
+def handler(signum = None, frame = None):
+    
+    oled.poweroff()
+    sys.exit(0)
+    
+for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+    signal.signal(sig, handler)   
+
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell = True )
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell = True )
+    cpu_usage=psutil.cpu_percent(interval=None)
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell = True )
+    
+    cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
+    temp = subprocess.check_output(cmd, shell = True )
+
+    # Pi Stats Display
+    draw.text((x, top+2), "IP: " + str(IP,'utf-8'), font=font, fill=255)
+    draw.text((x, top+18),  "CPU: "+str(cpu_usage)+"%" + " " + str(temp,'utf-8') , font=font, fill=255)
+    draw.text((x, top+34), str(MemUsage,'utf-8'), font=font, fill=255)
+    draw.text((x, top+50), str(Disk,'utf-8'), font=font, fill=255)
+
+    
+    # Display image
+    oled.image(image)
+    oled.show()
+    time.sleep(.1)
+
+
